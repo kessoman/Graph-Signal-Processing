14b8bfe7aaa4d613ebb3aad5881b2e03f267e893
kessoman <kessoman16@gmail.com>
2022-03-06 12:40:15 +0200

    GraphSignal Implementation

---
+++ src/GraphSignal.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class GraphSignal {
+	
+	protected HashMap<Node, Double> tempMap ;
+    
+	public GraphSignal(HashMap<Node, Double> tempMap) {
+		
+		
+	}
+	
+	public double getNodeScore (Node tempNode) {
+		return tempMap.get(tempNode);
+	}
+
+	public void setNodescore (Node newTempNode,Double tempDpuble) {
+		tempMap.put(newTempNode, tempDpuble);
+	}
+	
+}
