85d88da9191e1317682e76eff1cbf1493decacc5
kessoman <kessoman16@gmail.com>
2022-08-01 17:56:15 +0300

    Added implementations for addEdge() getNodes() & getNumberOfNodes in PartialDiscGraph

--- main/disc/PartialDiscGraph.java
+++ main/disc/PartialDiscGraph.java
@@ -1,13 +1,52 @@
 package disc;
 import core.*;
-
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.HashMap;
 
 public class PartialDiscGraph extends Graph{
+	private File file ;
+	private HashMap<String, Node> nodes = new HashMap<String, Node>();
+	public PartialDiscGraph(File file) {
+		this.file = file;
+	}
+	public void addEdge(Node sourceNode, Node destinationNode) {
+		if(!nodes.containsKey(sourceNode.toString()))
+			nodes.put(sourceNode.toString(), sourceNode);
+		if(!!nodes.containsKey(destinationNode.toString()))
+			nodes.put(destinationNode.toString(), destinationNode);
+		String s = System.lineSeparator() + sourceNode.toString() + " , " + destinationNode.toString();
+		boolean result;
+		Path p = file.toPath();
+		try {
+			result = file.createNewFile();
+			if (result) {
+				System.out.println("file created " + file.getCanonicalPath());
+				try {
+					Files.write(p, s.getBytes(), StandardOpenOption.APPEND);
+				} catch (IOException e) {
+					System.err.println(e);
+				}
+			} else {
+				System.out.println("File already exist at location: " + file.getCanonicalPath());
+				try {
+					Files.write(p, s.getBytes(), StandardOpenOption.APPEND);
+				} catch (IOException e) {
+					System.err.println(e);
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 	public Iterable<Edge> getEdges() {
 		throw new RuntimeException();
 	}
 	public Iterable<Node> getNodes() {
-		throw new RuntimeException();
+		return nodes.values();
 	}
 	public Iterable<Edge> getIncomingEdges(Node destinationnode) {
 		throw new RuntimeException();
@@ -26,14 +65,10 @@ public class PartialDiscGraph extends Graph{
 	}
 
 	public int getNumberOfNodes() {
-		throw new RuntimeException();
+		return nodes.size();
 	}
 
 	public int getNumberOfEdges() {
-		throw new RuntimeException();
-	}
-
-	public void addEdge(Node sourceNode, Node destinationNode) {
 		throw new RuntimeException();
 	}
 }
